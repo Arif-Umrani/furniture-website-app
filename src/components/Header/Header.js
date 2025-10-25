2a6a8b7ea66a755e0a043a408b508819358e9d44
Arif Ali <meer.aafay@gmail.com>
2025-10-25 07:47:16 -0700

    add header part complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Header.scss'
+import alertIcon from '../../assets/icons/account_alert.png'
+import searchIcon from '../../assets/icons/icons_search.png';
+import heartIcon from '../../assets/icons/icons_heart.png';
+import cartIcon from '../../assets/icons/shopping_cart.png';
+
+
+function Header() {
+  return (
+    // Nav-Bar
+    <div className="container">
+      <div className="nav-bar">
+        <div className="nav-items d-flex">
+          <a href="#!" className="nav-link">Home</a>
+          <a href="#!" className="nav-link">Shop</a>
+          <a href="#!" className="nav-link">About</a>
+          <a href="#!" className="nav-link">Contact</a>
+        </div>
+        <div className="nav-icon">
+          <img src={alertIcon} alt="home alert icon" />
+          <img src={searchIcon} alt="search icon" />
+          <img src={heartIcon} alt="heart icon" />
+          <img src={cartIcon} alt="cart icon" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
