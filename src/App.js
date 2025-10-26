666373ecbb1cc48693510e67dc27a92ca34e5716
Arif Ali <meer.aafay@gmail.com>
2025-10-25 22:37:39 -0700

    add home page - and landing banner

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
 
       <Header />
-
+      <Home />
       <Footer />
 
       {
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
+import './Home.scss';
+import landingImage from '../../src/assets/images/single_sofa.png';
 
 function Home () {
   return (
-    <div className="">
-
+    // Landing Banner Section
+    <section className="landing-banner">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-lg-6">
+          <h2>Rocket single  <br/> seater</h2><br/>
+          <h4>Shop Now</h4>
+        </div>
+        <div className="col-lg-6">
+          <img src={landingImage} alt="Sofa" />
+        </div>
+      </div>
     </div>
+    </section>
   )
 }
+
+export default Home;
